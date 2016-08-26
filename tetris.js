5eb1105a75a088d44fceb047c172b56862312d44
Pontus Persson <pom@spotify.com>
2016-08-26 12:34:49 +0200

    Refactor Tetris.

--- tetris.js
+++ tetris.js
@@ -3,112 +3,96 @@ const context = canvas.getContext('2d');
 
 const colors = [
     null,
-    '#FF0D72', // Redish
+    '#FF0D72',
     '#0DC2FF',
     '#0DFF72',
     '#F538FF',
     '#FF8E0D',
     '#FFE138',
     '#3877FF',
-    null,
-    null,
-    'grey',
 ];
 
 const patterns = ['SL','SR','line', 'LR', 'LL', 'box', 'T'];
-function getRandomPattern()
-{
-    return createPattern(patterns[Math.random() * patterns.length | 0]);
+
+function createMatrix(w, h) {
+    const matrix = [];
+    while(h--) {
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
 }
+
 
 function createPattern(type)
 {
-    const pat = new Pattern;
-
-    pat.pos[0] = arena.matrix[0].length / 2 | 0;
-
     if (type === 'line') {
-        pat.matrix = [
-            [0, 1, 0],
-            [0, 1, 0],
-            [0, 1, 0],
-            [0, 1, 0],
+        return [
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
         ];
     } else if (type === 'LR') {
-        pat.matrix = [
+        return [
             [0, 2, 0],
             [0, 2, 0],
             [0, 2, 2],
         ];
     } else if (type === 'LL') {
-        pat.matrix = [
+        return [
             [0, 3, 0],
             [0, 3, 0],
             [3, 3, 0],
         ];
     } else if (type === 'box') {
-        pat.matrix = [
+        return [
             [4, 4],
             [4, 4],
         ];
     } else if (type === 'SL') {
-        pat.matrix = [
+        return [
+            [0, 0, 0],
             [5, 5, 0],
             [0, 5, 5],
         ];
     } else if (type === 'SR') {
-        pat.matrix = [
+        return [
+            [0, 0, 0],
             [0, 6, 6],
             [6, 6, 0],
         ];
     } else if (type === 'T') {
-        pat.matrix = [
+        return [
             [0, 7, 0],
             [7, 7, 7],
             [0, 0, 0],
         ];
     }
-
-    return pat;
 }
 
-class Arena
-{
-    constructor(w, h)
-    {
-        this.matrix = [];
-        for (let x = 0; x < h; ++x) {
-            const row = [];
-            for (let y = 0; y < w; ++y) {
-                row.push(0);
+function collide(player, arena) {
+    const subset = matrixIntersect(arena, player.matrix,
+                                   player.pos[0], player.pos[1]);
+    for (let y = 0; y < subset.length; ++y) {
+        for (let x = 0; x < subset[y].length; ++x) {
+            if (subset[y][x] !== 0 && player.matrix[y][x] !== 0) {
+                return true;
             }
-            this.matrix.push(row);
         }
     }
+    return false;
 }
 
-class Pattern
-{
-    constructor()
-    {
-        this.pos = [0, 0];
-        this.scale = 10;
-        this.rotation = 0;
-        this.matrix = null;
+function draw() {
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, arena[0].length * scale, 600);
+
+    update(1/60);
+    drawMatrix(arena, [0, 0]);
+    if (player.matrix) {
+        drawMatrix(player.matrix, player.pos);
     }
-    rotate(dir)
-    {
-        this.matrix = this.matrix[0].map((col, i) => {
-            return this.matrix.map((row) => {
-                return row[i];
-            });
-        });
-        if (dir > 0) {
-            this.matrix = this.matrix.map(row => row.reverse());
-        } else {
-            this.matrix.reverse();
-        }
-    }
+    requestAnimationFrame(draw);
 }
 
 function drawMatrix(matrix, offset)
@@ -127,28 +111,44 @@ function drawMatrix(matrix, offset)
     });
 }
 
-const arena = new Arena(12, 20);
-
-let pat;
-let score = 0;
-let scale = 20;
-let dropDelay = 1;
-let dropCounter = 0;
-
-function draw() {
-    context.fillStyle = 'black';
-    context.fillRect(0, 0, arena.matrix[0].length * scale, 600);
-
-    update(1/60);
-    drawMatrix(arena.matrix, [0, 0]);
-    if (pat) {
-        drawMatrix(pat.matrix, pat.pos);
-    }
-    requestAnimationFrame(draw);
+function getRandomPattern()
+{
+    return createPattern(patterns[Math.random() * patterns.length | 0]);
 }
 
-function matrixIntersect(outer, inner, offsetX = 0, offsetY = 0)
-{
+function keyHandler(event) {
+    const k = event.keyCode;
+    if (event.type === 'keydown') {
+        if (k === 69 || k === 81) {
+            rotate(player.matrix, k === 69 ? 1 : -1);
+            let test = 1;
+            while (collide(player, arena)) {
+                player.pos[0] += test;
+                test = -(test + (test > 0 ? 1 : -1));
+
+                if (Math.abs(test) > 5) {
+                    throw new Error('Rotate offset overflow');
+                }
+            }
+        } else if (k === 65 || k === 68) {
+            const diff = k === 65 ? -1 : 1;
+            player.pos[0] += diff;
+            if (collide(player, arena)) {
+                player.pos[0] -= diff;
+            }
+        }
+    }
+
+    if (k === 83) {
+        if (event.type === 'keydown') {
+            dropDelay = .035;
+        } else {
+            dropDelay = 1;
+        }
+    }
+}
+
+function matrixIntersect(outer, inner, offsetX = 0, offsetY = 0) {
     return inner.map((row, y) => {
         return row.map((v, x) => {
             return outer[y + offsetY] && outer[y + offsetY][x + offsetX];
@@ -156,22 +156,7 @@ function matrixIntersect(outer, inner, offsetX = 0, offsetY = 0)
     });
 }
 
-function collide(pattern, arena)
-{
-    const subset = matrixIntersect(arena.matrix, pattern.matrix,
-                                   pattern.pos[0], pattern.pos[1]);
-    for (let y = 0; y < subset.length; ++y) {
-        for (let x = 0; x < subset[y].length; ++x) {
-            if (subset[y][x] !== 0 && pattern.matrix[y][x] !== 0) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-function merge(dest, source, offset)
-{
+function merge(dest, source, offset) {
     source.forEach((row, y) => {
         row.forEach((v, x) => {
             if (v === 0) {
@@ -182,6 +167,16 @@ function merge(dest, source, offset)
             dest[dY][dX] = v;
         });
     });
+}
+
+
+function rotate(matrix, dir) {
+  transpose(matrix);
+  if (dir > 0) {
+    matrix.forEach(row => row.reverse());
+  } else {
+    matrix.reverse();
+  }
 }
 
 function sweep(matrix)
@@ -198,73 +193,72 @@ function sweep(matrix)
             const row = matrix.splice(y, 1)[0].fill(0);
             matrix.unshift(row);
             ++y;
-            score += sweepCount * 10;
+            player.score += sweepCount * 10;
             updateScore();
             sweepCount *= 2;
         }
     }
 }
 
-function updateScore() {
-    document.getElementById('score').innerText = score;
+function transpose(matrix) {
+  for (let y = 0; y < matrix.length; ++y) {
+    for (let x = 0; x < y; ++x) {
+      [
+        matrix[x][y],
+        matrix[y][x],
+      ] = [
+        matrix[y][x],
+        matrix[x][y],
+      ];
+    }
+  }
 }
 
 function update(dt) {
-    if (!pat) {
+    if (!player.matrix) {
         updateScore();
-        pat = getRandomPattern();
-        if (collide(pat, arena)) {
+        player.matrix = getRandomPattern();
+        player.pos = [
+            (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0),
+            0,
+        ];
+        if (collide(player, arena)) {
             score = 0;
-            arena.matrix.forEach(row => row.fill(0));
+            arena.forEach(row => row.fill(0));
         }
     }
 
     dropCounter += dt;
     if (dropCounter > dropDelay) {
-        pat.pos[1]++;
+        player.pos[1]++;
         dropCounter = 0;
-        if (collide(pat, arena)) {
-            pat.pos[1]--;
-            merge(arena.matrix, pat.matrix, pat.pos);
-            sweep(arena.matrix);
-            pat = null;
+        if (collide(player, arena)) {
+            player.pos[1]--;
+            merge(arena, player.matrix, player.pos);
+            sweep(arena);
+            player.matrix = null;
         }
     }
 }
 
-draw();
-
-const keyHandler = event => {
-    const k = event.keyCode;
-    if (event.type === 'keydown') {
-        if (k === 69 || k === 81) {
-            pat.rotate(k === 69 ? 1 : -1);
-            let test = 1;
-            while (collide(pat, arena)) {
-                pat.pos[0] += test;
-                test = -(test + (test > 0 ? 1 : -1));
-
-                if (Math.abs(test) > 5) {
-                    throw new Error('Rotate offset overflow');
-                }
-            }
-        } else if (k === 65 || k === 68) {
-            const diff = k === 65 ? -1 : 1;
-            pat.pos[0] += diff;
-            if (collide(pat, arena)) {
-                pat.pos[0] -= diff;
-            }
-        }
-    }
-
-    if (k === 83) {
-        if (event.type === 'keydown') {
-            dropDelay = .035;
-        } else {
-            dropDelay = 1;
-        }
-    }
-};
+function updateScore() {
+    document.getElementById('score').innerText = player.score;
+}
 
 document.addEventListener('keydown', keyHandler);
 document.addEventListener('keyup', keyHandler);
+
+let dropDelay = 1;
+let dropCounter = 0;
+
+const scale = 20;
+const arena = createMatrix(12, 20);
+const player = {
+    score: 0,
+    pos: [0, 0],
+    matrix: null,
+}
+
+draw();
+
+
