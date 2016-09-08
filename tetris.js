e6d3c1c58e600ad40a1726dfc51687b9fc37f308
Pontus Persson <pom@spotify.com>
2016-09-08 16:10:26 +0200

    Add rotate function.

--- tetris.js
+++ tetris.js
@@ -70,6 +70,26 @@ function merge(arena, player) {
     });
 }
 
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ];
+        }
+    }
+
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
+}
+
 function playerDrop() {
     player.pos.y++;
     if (collide(arena, player)) {
