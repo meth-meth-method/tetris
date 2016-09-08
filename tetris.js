89aa2196eb54929b7564f4be9b01798ea31fb8c4
Pontus Persson <pom@spotify.com>
2016-09-08 16:57:07 +0200

    Add score counting.

--- tetris.js
+++ tetris.js
@@ -4,6 +4,7 @@ const context = canvas.getContext('2d');
 context.scale(20, 20);
 
 function arenaSweep() {
+    let rowCount = 1;
     outer: for (let y = arena.length -1; y > 0; --y) {
         for (let x = 0; x < arena[y].length; ++x) {
             if (arena[y][x] === 0) {
@@ -14,6 +15,9 @@ function arenaSweep() {
         const row = arena.splice(y, 1)[0].fill(0);
         arena.unshift(row);
         ++y;
+
+        player.score += rowCount * 10;
+        rowCount *= 2;
     }
 }
 
@@ -145,6 +149,7 @@ function playerDrop() {
         merge(arena, player);
         playerReset();
         arenaSweep();
+        updateScore();
     }
     dropCounter = 0;
 }
@@ -164,6 +169,8 @@ function playerReset() {
                    (player.matrix[0].length / 2 | 0);
     if (collide(arena, player)) {
         arena.forEach(row => row.fill(0));
+        player.score = 0;
+        updateScore();
     }
 }
 
@@ -200,6 +207,10 @@ function update(time = 0) {
     requestAnimationFrame(update);
 }
 
+function updateScore() {
+    document.getElementById('score').innerText = player.score;
+}
+
 document.addEventListener('keydown', event => {
     if (event.keyCode === 37) {
         playerMove(-1);
@@ -230,7 +241,9 @@ const arena = createMatrix(12, 20);
 const player = {
     pos: {x: 0, y: 0},
     matrix: null,
+    score: 0,
 };
 
 playerReset();
+updateScore();
 update();
