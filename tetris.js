abcf2f4694f1efc275ac827b00eb13a10aab2e4f
Pontus Persson <pom@spotify.com>
2016-09-08 14:09:58 +0200

    Add matrix for T piece and draw tiny.

--- tetris.js
+++ tetris.js
@@ -3,3 +3,18 @@ const context = canvas.getContext('2d');
 
 context.fillStyle = '#000';
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+const matrix = [
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+];
+
+matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+        if (value !== 0) {
+            context.fillStyle = 'red';
+            context.fillRect(x, y, 1, 1);
+        }
+    });
+});
