2393af85cc669913aae476d9faa63e2622f78252
Pontus Persson <pom@spotify.com>
2016-09-08 14:12:29 +0200

    Change context scale.

--- tetris.js
+++ tetris.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
+context.scale(20, 20);
+
 context.fillStyle = '#000';
 context.fillRect(0, 0, canvas.width, canvas.height);
 
