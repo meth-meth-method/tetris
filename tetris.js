06e7c83ebbd6e2102e1499bd7a8f07e071dfdb46
Pontus Persson <pom@spotify.com>
2016-09-08 14:21:13 +0200

    Add offset support to drawMatrix().

--- tetris.js
+++ tetris.js
@@ -12,15 +12,17 @@ const matrix = [
     [0, 0, 0],
 ];
 
-function drawMatrix(matrix) {
+function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
                 context.fillStyle = 'red';
-                context.fillRect(x, y, 1, 1);
+                context.fillRect(x + offset.x,
+                                 y + offset.y,
+                                 1, 1);
             }
         });
     });
 }
 
-drawMatrix(matrix);
+drawMatrix(matrix, {x: 5, y: 5});
