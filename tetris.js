414faae44afaff2080c45ba926552744a7b9daaf
Pontus Persson <pom@spotify.com>
2016-09-08 16:51:47 +0200

    Add arenaSweep.

--- tetris.js
+++ tetris.js
@@ -3,6 +3,20 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
+function arenaSweep() {
+    outer: for (let y = arena.length -1; y > 0; --y) {
+        for (let x = 0; x < arena[y].length; ++x) {
+            if (arena[y][x] === 0) {
+                continue outer;
+            }
+        }
+
+        const row = arena.splice(y, 1)[0].fill(0);
+        arena.unshift(row);
+        ++y;
+    }
+}
+
 function collide(arena, player) {
     const m = player.matrix;
     const o = player.pos;
@@ -130,6 +144,7 @@ function playerDrop() {
         player.pos.y--;
         merge(arena, player);
         playerReset();
+        arenaSweep();
     }
     dropCounter = 0;
 }
