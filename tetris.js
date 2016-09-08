a07f10801e963c659c33d99fcf356c86146a1ee7
Pontus Persson <pom@spotify.com>
2016-09-08 15:56:33 +0200

    Add merge function.

--- tetris.js
+++ tetris.js
@@ -44,6 +44,16 @@ function draw() {
     drawMatrix(player.matrix, player.pos);
 }
 
+function merge(arena, player) {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                arena[y + player.pos.y][x + player.pos.x] = value;
+            }
+        });
+    });
+}
+
 function playerDrop() {
     player.pos.y++;
     dropCounter = 0;
