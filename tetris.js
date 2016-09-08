4af3a2ccba7457f453ad2aa6919b76fb5b6e0281
Pontus Persson <pom@spotify.com>
2016-09-08 15:57:49 +0200

    Add collide function.

--- tetris.js
+++ tetris.js
@@ -9,6 +9,21 @@ const matrix = [
     [0, 0, 0],
 ];
 
+function collide(arena, player) {
+    const m = player.matrix;
+    const o = player.pos;
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if (m[y][x] !== 0 &&
+               (arena[y + o.y] &&
+                arena[y + o.y][x + o.x]) !== 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 function createMatrix(w, h) {
     const matrix = [];
     while (h--) {
