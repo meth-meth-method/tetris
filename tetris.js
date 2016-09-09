3ca56218f01700e984f92272ce4ae56f8663aece
Pontus Persson <pom@spotify.com>
2016-09-09 08:36:56 +0200

    Use Array.some and Array.every to simplify loops.

--- tetris.js
+++ tetris.js
@@ -5,35 +5,26 @@ context.scale(20, 20);
 
 function arenaSweep() {
     let rowCount = 1;
-    outer: for (let y = arena.length -1; y > 0; --y) {
-        for (let x = 0; x < arena[y].length; ++x) {
-            if (arena[y][x] === 0) {
-                continue outer;
-            }
+    for (let y = arena.length -1; y > 0; --y) {
+        if (arena[y].every(v => v !== 0)) {
+            const row = arena.splice(y, 1)[0].fill(0);
+            arena.unshift(row);
+            ++y;
+
+            player.score += rowCount * 10;
+            rowCount *= 2;
         }
-
-        const row = arena.splice(y, 1)[0].fill(0);
-        arena.unshift(row);
-        ++y;
-
-        player.score += rowCount * 10;
-        rowCount *= 2;
     }
 }
 
 function collide(arena, player) {
-    const m = player.matrix;
-    const o = player.pos;
-    for (let y = 0; y < m.length; ++y) {
-        for (let x = 0; x < m[y].length; ++x) {
-            if (m[y][x] !== 0 &&
-               (arena[y + o.y] &&
-                arena[y + o.y][x + o.x]) !== 0) {
-                return true;
-            }
-        }
-    }
-    return false;
+    return player.matrix.some((row, y) => {
+        return row.some((value, x) => {
+            return value !== 0 &&
+                (arena[y + player.pos.y] &&
+                arena[y + player.pos.y][x + player.pos.x]) !== 0;
+        });
+    });
 }
 
 function createMatrix(w, h) {
